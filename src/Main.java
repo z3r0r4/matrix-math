a799a8dea67b6f2b51387f612bb874d629905229
z3r0r4 <R4C1.meru@protonmail.com>
2018-07-23 15:03:06 +0200

    changed .gitignore / enabled automatic JavaDoc builds

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 
 public class Main {
-	
+	/*
+	 * Demonstration
+	 * */
 	public static void main(String[] args) {
-//		 Matrix A = new Matrix(0,0,1,10);
-//		 Matrix B = new Matrix(A);
-//		 Matrix.printM(A);
-//		 Matrix.printM(B);
-//		 A.negate();
-//		 Matrix.printM(A);
-//		 Matrix.printM(B);
-//		 A.add(B);
-//		 Matrix.printM(A);
+		 Matrix A = new Matrix(0,0,1,10);
+		 Matrix B = new Matrix(A);
+		 Matrix.printM(A);
+		 Matrix.printM(B);
+		 A.negate();
+		 Matrix.printM(A);
+		 Matrix.printM(B);
+		 A.add(B);
+		 Matrix.printM(A);
 		
 //TODO mapping lambda Function 
 		 //sigmoid or something like thathahthathhath
