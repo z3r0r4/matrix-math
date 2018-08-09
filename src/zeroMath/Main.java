d2b58ee380a39eb0f98a62fe197401e14107f7bb
unknown <R4C1.meru@protonmail.com>
2018-08-09 23:57:39 +0200

    added info methode/ removed extensive console logs

--- src/zeroMath/Main.java
+++ src/zeroMath/Main.java
@@ -1,19 +1,11 @@
 package zeroMath;
 
 public class Main {
-	/*
-	 * Demonstration
-	 */
+
 	public static void main(String[] args) {
 		Matrix A = new Matrix(0, 0, 1, 10);
-//		Matrix B = new Matrix(A);
-//		Matrix.printM(A);
-//		Matrix.printM(B);
-//		A.negate();
-//		Matrix.printM(A);
-//		Matrix.printM(B);
-//		A.add(B);
-//		Matrix.printM(A);
 		A.setData(1, 0, 2);
+		Matrix.printM(A);
+		A.info(null);
 	}
 }
